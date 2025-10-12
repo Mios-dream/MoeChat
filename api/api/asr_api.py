ef3a33746323d091ada7c917e820c025951b515d
Mio-dream <2814721302@qq.com>
2025-10-12 15:02:40 +0800

    将asr语音解码改为标准base64格式

--- api/api/asr_api.py
+++ api/api/asr_api.py
@@ -24,59 +24,65 @@ async def asr_audio(params: asr_data):
     return text
 
 
-# vad接口
 @asr_api.websocket("/asr_ws")
 async def asr_websocket(c_websocket: WebSocket):
     await c_websocket.accept()
-    # state = np.zeros((2, 1, 128), dtype=np.float32)
-    # sr = np.array(16000, dtype=np.int64)
-    # 初始化 VAD 迭代器，指定采样率为 16000Hz
+    print("WebSocket 连接已建立")
+
     vad_iterator = VADIterator(speech_pad_ms=300)
     current_speech = []
     current_speech_tmp = []
     status = False
+
     while True:
         try:
             data = await c_websocket.receive_text()
             data = json.loads(data)
+
             if data["type"] == "asr":
-                audio_data = base64.urlsafe_b64decode(str(data["data"]).encode("utf-8"))
+                # 改为标准 base64 解码
+                audio_data = base64.b64decode(str(data["data"]).encode("utf-8"))
                 samples = np.frombuffer(audio_data, dtype=np.int16)
+
                 current_speech_tmp.append(samples)
+
                 if len(current_speech_tmp) < 4:
                     continue
+
                 resampled = np.concatenate(current_speech_tmp.copy())
                 resampled = (resampled / 32768.0).astype(np.float32)
                 current_speech_tmp = []
 
                 for speech_dict, speech_samples in vad_iterator(resampled):
+
                     if "start" in speech_dict:
+
                         current_speech = []
                         status = True
-                        pass
+
                     if status:
                         current_speech.append(speech_samples)
                     else:
                         continue
+
                     is_last = "end" in speech_dict
                     if is_last:
+
                         status = False
                         combined = np.concatenate(current_speech)
                         audio_bytes = b""
                         with BytesIO() as buffer:
                             sf.write(
-                                buffer,
-                                combined,
-                                16000,
-                                format="WAV",
-                                subtype="PCM_16",
+                                buffer, combined, 16000, format="WAV", subtype="PCM_16"
                             )
                             buffer.seek(0)
-                            audio_bytes = buffer.read()  # 完整的 WAV bytes
+                            audio_bytes = buffer.read()
                             res_text = chat_core.asr(audio_bytes)
+
                             if res_text:
                                 await c_websocket.send_text(res_text)
-                        current_speech = []  # 清空当前段落
+                        current_speech = []
 
-        except:
+        except Exception as e:
+            print(f"错误: {e}")
             break
