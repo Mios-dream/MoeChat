b54f937036b4b4f00efdc42bfec7374a4da2b261
AlfreScarlet <659186445@qq.com>
2025-10-20 21:26:16 +0800

    添加socket接口，删除冗余代码

--- main_web.py
+++ main_web.py
@@ -2,11 +2,15 @@ import asyncio
 from init_server import init
 from web.src.router.router import app
 import uvicorn
+from threading import Thread
+from api.api.asr_api import start_socket_server
 
 
 def start_server():
     # 等待初始化完成
     asyncio.run(init())
+    # 启动socket服务
+    Thread(target=start_socket_server, args=("0.0.0.0", 8002), daemon=True).start()
     # 启动web服务，应该在最后
     uvicorn.run(app, host="0.0.0.0", port=8001)
 
